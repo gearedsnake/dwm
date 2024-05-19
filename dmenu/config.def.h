6274f4e9174c6dfc7873b4f206215caab0e7d311
Terack <112418469+terackYT@users.noreply.github.com>
2024-05-19 12:32:03 +0000

    Add files via upload

---
+++ dmenu/config.def.h
@@ -0,0 +1,29 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 250;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"0xProtoNerdFont:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ffffff", "#0f1214" },
+	[SchemeSel] = { "#0f1214", "#ffffff" },
+	[SchemeOut] = { "#ffffff", "#020203" },
+};
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 15;
+static unsigned int columns    = 1;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 3;
